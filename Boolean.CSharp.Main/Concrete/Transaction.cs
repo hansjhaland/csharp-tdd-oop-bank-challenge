127d900ee2f90808f23d136e2bf19f1040070ad9
Hans Jakob Håland <Hans.Jakob.Haland@no.experis.com>
2025-08-13 11:10:18 +0200

    Adds tests and functionalty for transaction object creation

--- Boolean.CSharp.Main/Concrete/Transaction.cs
+++ Boolean.CSharp.Main/Concrete/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Boolean.CSharp.Main.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace Boolean.CSharp.Main.Concrete
 {
     public class Transaction
     {
+        private TransactionType _type;
+        private decimal _amount; // Assume absolute value
+        private decimal _resultingBalance;
+
+        public Transaction(decimal amount, decimal currentBalance)
+        {
+            _amount = Math.Abs(amount);
+            _type = amount > 0 ? TransactionType.Credit : TransactionType.Debit;
+            _resultingBalance = currentBalance + amount;
+        }
+
+        public TransactionType Type { get { return _type; } }
+        public decimal Amount { get { return _amount; } }
+        public decimal ResultingBalance
+        {
+            get { return _resultingBalance; }
+        }
     }
 }
--- Boolean.CSharp.Test/CoreTests.cs
+++ Boolean.CSharp.Test/CoreTests.cs
@@ -1,5 +1,6 @@
 ﻿using Boolean.CSharp.Main;
 using Boolean.CSharp.Main.Abstract;
+using Boolean.CSharp.Main.Concrete;
 using Boolean.CSharp.Main.Concrete.Accounts;
 using Boolean.CSharp.Main.Enums;
 using NUnit.Framework;
@@ -24,6 +25,26 @@ namespace Boolean.CSharp.Test
             Assert.That(savingsAccount.Branch, Is.EqualTo(Branch.Trondheim));
             Assert.True(currentAccount is BankAccount);
             Assert.True(savingsAccount is BankAccount);
+        }
+
+        [Test]
+        public void CreateTransactionTest()
+        {
+            decimal currentBalance = 500;
+            decimal depositAmount = 100;
+            decimal withdrawAmount = -100;
+            decimal resultingBalanceDeposit = currentBalance + depositAmount;
+            decimal resultingBalanceWithdraw = currentBalance + withdrawAmount;
+
+            Transaction transactionCredit = new Transaction(depositAmount, currentBalance);
+            Transaction transactionDebit = new Transaction(withdrawAmount, currentBalance);
+
+            Assert.That(transactionCredit.Amount, Is.EqualTo(depositAmount));
+            Assert.That(transactionDebit.Amount, Is.EqualTo(Math.Abs(withdrawAmount)));
+            Assert.That(transactionCredit.ResultingBalance, Is.EqualTo(resultingBalanceDeposit));
+            Assert.That(transactionDebit.ResultingBalance, Is.EqualTo(resultingBalanceWithdraw));
+            Assert.That(transactionDebit.Type, Is.EqualTo(TransactionType.Debit));
+            Assert.That(transactionCredit.Type, Is.EqualTo(TransactionType.Credit));
         }
 
     }
