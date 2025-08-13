4856f3eda813dd568fa7f54655496ff02e58c711
Hans Jakob Håland <Hans.Jakob.Haland@no.experis.com>
2025-08-13 15:17:31 +0200

    Adds bankstatment via SMS with Twilio

---
+++ TwilioSend/Program.cs
@@ -0,0 +1,35 @@
+﻿// Install the C# / .NET helper library from twilio.com/docs/csharp/install
+
+using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using System.Threading.Tasks;
+using Boolean.CSharp.Main.Concrete.Accounts;
+using Boolean.CSharp.Main.Enums;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        // Find your Account SID and Auth Token at twilio.com/console
+        // and set the environment variables. See http://twil.io/secure
+        string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+        string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+        TwilioClient.Init(accountSid, authToken);
+
+        CurrentAccount currentAccount = new CurrentAccount(Branch.Oslo);
+        currentAccount.AddTransaction(1000);
+        currentAccount.AddTransaction(2000);
+        currentAccount.AddTransaction(-500);
+
+        string bankStatement = currentAccount.GenerateBankStatement();
+
+        var message = await MessageResource.CreateAsync(
+            body: bankStatement,
+            from: new Twilio.Types.PhoneNumber("+15017122661"),
+            to: new Twilio.Types.PhoneNumber("+15558675310"));
+
+        Console.WriteLine(message.Body);
+    }
+}
