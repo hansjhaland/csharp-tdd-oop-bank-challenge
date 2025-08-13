746c3ff07391fa852712c73156d34283875fe0f7
Hans Jakob Håland <Hans.Jakob.Haland@no.experis.com>
2025-08-13 10:20:50 +0200

    Adds abstract and concrete classes and enums

---
+++ Boolean.CSharp.Main/Concrete/Accounts/CurrentAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main.Concrete.Accounts
+{
+    public class CurrentAccount
+    {
+    }
+}
---
+++ Boolean.CSharp.Main/Enums/Enums.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean.CSharp.Main.Enums
+{
+    public enum Branch
+    {
+        Oslo,
+        Bergen,
+        Trondheim
+    }
+
+    public enum Status
+    {
+        Pending,
+        Approved,
+        Rejected
+    }
+
+    public enum TransactionType
+    {
+        Credit,
+        Debit
+    }
+}
